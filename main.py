0dd4262bcde62e3cb2d88f766f99bd03a098b3bb
lukasbuse4 <lukasbuse4@gmail.com>
2022-12-08 06:58:00 -0700

    Changed name to IsaFlowers

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Addy RollFrogs')
+    print_hi('Isa_Flowers')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
